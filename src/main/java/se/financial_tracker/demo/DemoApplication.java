b46b697856d162eef7896d91d69f0d0533176a80
martiningeson <oi12min@cs.umu.se>
2026-08-20 21:59:47 +0200

    Rename: project/folders - 'tracking' to 'tracker'

--- src/main/java/se/financial_tracker/demo/DemoApplication.java
+++ src/main/java/se/financial_tracker/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package se.financial_tracking.demo;
+package se.financial_tracker.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
